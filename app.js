bab5e51a22c2d5fb9f10c38fb5b57fa68d77f586
yuyang <tianxiang.htx@alibaba-inc.com>
2021-06-07 15:30:13 +0800

    feat: init to #33591059

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+
+
 import { onAuthAppBack } from 'dingtalk-design-libs/biz/openAuthMiniApp';
 App({
   onLaunch(options) {
     console.log('App Launch', options);
+    console.log('getSystemInfoSync', dd.getSystemInfoSync());
+    console.log('SDKVersion', dd.SDKVersion);
+    this.globalData.corpId = options.query.corpId;
   },
   onShow(options) {
+    console.log('App Show');
     onAuthAppBack(options, (data) => {
       // 这里可以对返回数据做二次处理，之后需要把数据返回到page.onShow
       dd.alert({
--- page/index/index.js
+++ page/index/index.js
@@ -1,100 +1,126 @@
-import { openAuthMiniApp, disposeAuthData} from 'dingtalk-design-libs/biz/openAuthMiniApp';
-// const app = getApp();
+import {
+  openAuthMiniApp,
+  disposeAuthData,
+} from "dingtalk-design-libs/biz/openAuthMiniApp";
+let app = getApp();
+
+//内网穿透工具介绍:
+// https://open-doc.dingtalk.com/microapp/debug/ucof2g
+//替换成开发者后台设置的安全域名
+let domain = "http://localhost:3000";
+let url = domain + "/login";
 
 Page({
-    data:{
-    },
-    onShow(e) {
-      disposeAuthData((options)=>{
+  data: {
+    corpId: "",
+    authCode: "",
+    userId: "",
+    userName: "",
+    hideList: true,
+  },
+  loginSystem() {
+    dd.showLoading();
+    dd.getAuthCode({
+      success: (res) => {
+        this.setData({
+          authCode: res.authCode,
+        });
+        //dd.alert({content: "step1"});
+        dd.httpRequest({
+          url: url,
+          method: "POST",
+          data: {
+            authCode: res.authCode,
+          },
+          dataType: "json",
+          success: (res) => {
+            // dd.alert({content: "step2"});
+            console.log("success----", res);
+            let userId = res.data.result.userId;
+            let userName = res.data.result.userName;
+            this.setData({
+              userId: userId,
+              userName: userName,
+              hideList: false,
+            });
+          },
+          fail: (res) => {
+            console.log("httpRequestFail---", res);
+            dd.alert({ content: JSON.stringify(res) });
+          },
+          complete: (res) => {
+            dd.hideLoading();
+          },
+        });
+      },
+      fail: (err) => {
+        // dd.alert({content: "step3"});
         dd.alert({
-          title:'disposeAuthData',
-          content:JSON.stringify(options)
-        })
-      })
-    },
-    onTap() {
-      const path = 'pages/home/home';
-      return openAuthMiniApp({
-          appId: '5000000000174977', //'5000000000371217', //5000000000174977',
-          path,
-          panelHeight: 'percent50',
-/*          deployVersion: '0.1.2105251749.7037',
-          protocol: '1.0',
-          buildId:'2392045',*/
-          extraData:{
-            clientId:'dingwlanwvdmrtjjwdmd',
-            clientType:0,
-            corpId:'ding9f50b15bccd16741',
-            rpcScope:'Contact.User.Read',
-            fieldScope:'Contact.User.mobile',
-            type:0,
-            ext: JSON.stringify({}),
-            from:''
-          }
-      });
-    },
-  onOrgTap() {
-    const path = 'pages/home/home'; //'page/index/index';
-    return openAuthMiniApp({
-      appId: '5000000000174977', //'5000000000371217', //5000000000174977',
-      path,
-      panelHeight: 'percent50',
-      /*          deployVersion: '0.1.2105251749.7037',
-                protocol: '1.0',
-                buildId:'2392045',*/
-      extraData:{
-        clientId:'suitejtxqmzlvvubpokgq', //'dingwlanwvdmrtjjwdmd',
-        clientType:0,
-        corpId:'ding027f45142432fbd135c2f4657eb6378f', //'ding9f50b15bccd16741',
-        rpcScope:'Contact.ManagementGroup.Read', //'Contact.User.Read',
-        //fieldScope:'Contact.User.mobile',
-        type:0,
-        ext: JSON.stringify({}),
-        from:''
-      }
+          content: JSON.stringify(err),
+        });
+      },
     });
   },
-    onTapMini() {
-      const path = 'page/index/index';
-      dd.navigateToMiniProgram({
-        appId: '5000000000371217',
-        ddAppParams: {
-          ddMode: 'float',
-          panelHeight: 'percent75'
-        },
-        path: path,
-        extraData: {
-          clientId:'dingwlanwvdmrtjjwdmd',
-          clientType:0,
-          corpId:'ding9f50b15bccd16741',
-          rpcScope:'Contact.User.Read',
-          fieldScope:'Contact.User.mobile',
-          type:0,
-          ext:JSON.stringify({})
-        },
-        success (res) {
-          dd.alert({
-            content: res,
-          });
-          console.log("navigateToMiniProgram succ" + JSON.stringify(res));
-        },
-        fail (err) {
-          dd.alert({content: "navigateToMiniProgram fail" + JSON.stringify(err),});
-        }
-      });
-    },
+  onLoad() {
+    let _this = this;
 
-    onCancelTap(){
-      return openAuthMiniApp({
-        float:false,
-/*        deployVersion: '0.1.2104091638.17536',
-        protocol: '1.0',
-        buildId:'2279609',*/
-        extraData:{
-          clientId:'dingwlanwvdmrtjjwdmd',
-          ext: JSON.stringify({}),
-          from:'aaa'
-        }
+    this.setData({
+      corpId: app.globalData.corpId,
+    });
+    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+    //dd.alert({content: "step1"});
+  },
+  onShow(e) {
+    disposeAuthData((options) => {
+      dd.alert({
+        title: "disposeAuthData",
+        content: JSON.stringify(options),
       });
-    }
+    });
+  },
+  openCancelPage() {
+    return openAuthMiniApp({
+      path: "pages/cancel/index",  // 不要改
+      panelHeight: "percent50",
+      extraData: {
+        clientId: "dingwlanwvdmrtjjwdmd", // 应用ID(唯一标识)
+        corpId: "ding9f50b15bccd16741", //三方企业ID
+        rpcScope: "Contact.User.Read",
+        fieldScope: "Contact.User.mobile",
+        type: 0,
+        ext: JSON.stringify({}),
+        from: "",
+      },
+      success: (res) => {
+        console.log(JSON.stringify(res));
+      },
+      fail: (res) => {
+        console.log(JSON.stringify(res));
+      },
+    });
+  },
+  openApp() {
+    console.log("openAuthMiniApp", openAuthMiniApp);
+    return openAuthMiniApp({
+      path: "pages/home/home", // 不要改
+      panelHeight: "percent50",
+      extraData: {
+        clientId: "dingwlanwvdmrtjjwdmd", // 应用ID(唯一标识)
+        corpId: "ding9f50b15bccd16741", //三方企业ID
+        rpcScope: "Contact.User.Read",
+        fieldScope: "Contact.User.mobile",
+        type: 0,
+        ext: JSON.stringify({}),
+        from: "",
+      },
+    });
+  },
+  onShareAppMessage() {
+    // 返回自定义分享信息
+    return {
+      title: "My App",
+      desc: "My App description",
+      path: "pages/index/index",
+    };
+  },
 });
